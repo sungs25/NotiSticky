c359eae90f185c5273bc74cce527f14bbaaf30ad
Sungmin <lsm0205l56@gmail.com>
2026-02-18 17:14:41 +0900

    feat: MainActivity 화면 설정, NotiStickyApp 구현, 매니페스트 설정

--- app/src/main/java/com/example/notisticky/MainActivity.kt
+++ app/src/main/java/com/example/notisticky/MainActivity.kt
@@ -3,45 +3,41 @@ package com.example.notisticky
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.notisticky.ui.home.HomeScreen
 import com.example.notisticky.ui.theme.NotiStickyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint // 1. Hilt 시작점
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+            // 테마 설정 (기본 생성된 코드 있으면 그거 쓰시고, 없으면 생략 가능)
             NotiStickyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // 2. 운전대 생성
+                val navController = rememberNavController()
+
+                // 3. 라우터 설정 (여기서 길을 안내합니다)
+                NavHost(navController = navController, startDestination = "home") {
+
+                    // URL 1: "home"
+                    composable("home") {
+                        HomeScreen() {
+                            navController.navigate("add")
+                        }
+                    }
+
+                    // URL 2: "add"
+                    composable("add") {
+                        Text("여기는 메모 작성 화면")
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotiStickyTheme {
-        Greeting("Android")
-    }
-}
