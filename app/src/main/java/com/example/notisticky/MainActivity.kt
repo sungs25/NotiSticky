561e4619678b0331be37f1bb0a155f29029d7d93
Sungmin <lsm0205l56@gmail.com>
2026-02-18 17:27:47 +0900

    debug: 에뮬레이터 화면 안 뜨는 문제 해결 및 애드몹 테스트 id 추가

--- app/src/main/java/com/example/notisticky/MainActivity.kt
+++ app/src/main/java/com/example/notisticky/MainActivity.kt
@@ -11,27 +11,27 @@ import com.example.notisticky.ui.home.HomeScreen
 import com.example.notisticky.ui.theme.NotiStickyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint // 1. Hilt 시작점
+@AndroidEntryPoint //Hilt 시작점
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // 테마 설정 (기본 생성된 코드 있으면 그거 쓰시고, 없으면 생략 가능)
+            // 테마 설정
             NotiStickyTheme {
-                // 2. 운전대 생성
+                // 운전대 생성
                 val navController = rememberNavController()
 
-                // 3. 라우터 설정 (여기서 길을 안내합니다)
+                // 라우터 설정
                 NavHost(navController = navController, startDestination = "home") {
 
-                    // URL 1: "home"
+                    // URL 1: home
                     composable("home") {
                         HomeScreen() {
                             navController.navigate("add")
                         }
                     }
 
-                    // URL 2: "add"
+                    // URL 2: add
                     composable("add") {
                         Text("여기는 메모 작성 화면")
                     }
